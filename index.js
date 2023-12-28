49e0ef7f6685f6bda956ed32d0e2a0a291bdcf67
MinhVu <phanminhvu1997@gmail.com>
2023-12-28 11:05:55 +0700

    ud

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import multer from 'multer';
 import xlsx from 'xlsx';
 import fs from 'fs';
-
+import cors from 'cors';
 const app = express();
-
+app.use(cors({origin: '*'}));
 // Set up Multer to handle file uploads
 const storage = multer.memoryStorage();
 const upload = multer({storage: storage});
@@ -19,15 +19,76 @@ app.post('/upload', upload.single('file'), (req, res) => {
     const sheetName = workbook.SheetNames[0]; // Assuming only one sheet for simplicity
     const worksheet = workbook.Sheets[sheetName];
 
-    let data = xlsx.utils.sheet_to_json(worksheet, {range:1});
+    let data = xlsx.utils.sheet_to_json(worksheet, {range: 1});
     // data = data.slice(3);
     // Delete the first property of every object in arr
     data.forEach(obj => delete obj[Object.keys(obj)[0]]);
 
+    function validateForm(fields) {
+
+        const fullName = fields['Họ và tên'];
+        const dayOfBirth = fields['Ngày sinh'];
+        const monthOfBirth = fields['Tháng sinh'];
+        const yearOfBirth = fields['Năm sinh'];
+        const gender = fields['Giới Tính'];
+        const idNumber = fields['Số CCCD/CMT\\r\\n(nếu có)'];
+        const grade = fields['Khối'];
+        const className = fields['Lớp'];
+        const fatherFullName = fields['Họ và tên phụ huynh thí sinh\\r\\n(Bố)'];
+        const motherFullName = fields['Họ và tên phụ huynh thí sinh\\r\\n(Mẹ)'];
+        const email = fields['Email'];
+        const parentPhoneNumber = fields['Số điện thoại của Phụ huynh\r\n(bắt buộc)'];
+
+        // Validation rules
+        const isNumber = /^\d+$/;
+        const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        // Validation logic for each field
+        const isFullNameValid = !!fullName; // Example: Must not be empty
+
+        const isDateOfBirthValid = (dayOfBirth <= 31 && [1, 3, 5, 7, 8, 10, 12].includes(monthOfBirth)) ||
+            (dayOfBirth <= 30 && [4, 6, 9, 11].includes(monthOfBirth)) ||
+            (dayOfBirth <= 29 && monthOfBirth === 2);
+
+        const isMonthOfBirthValid = monthOfBirth <= 12;
+
+        const isYearOfBirthValid = yearOfBirth >= 1900;
+
+        const isGenderValid = gender === 0 || gender === 1;
+
+        const isIdNumberValid = !idNumber || isNumber.test(idNumber);
+
+        const isGradeValid = grade <= 12;
+
+        // Other validations for each field...
+        const phoneRegex = /^(0|\+84)(\d{9,10})$/;
+
+        // Validation for parentPhoneNumber (specifically <= 10 digits)
+        const isParentPhoneNumberValid = parentPhoneNumber.length <= 10 && isNumber.test(parentPhoneNumber) && phoneRegex.test(parentPhoneNumber);
+        // Check if all validations pass
+        const isValid = isFullNameValid && isDateOfBirthValid && isMonthOfBirthValid &&
+            isYearOfBirthValid && isGenderValid && isIdNumberValid && isGradeValid &&
+            isParentPhoneNumberValid;
+        return isValid;
+
+    }
+// Condition: Even numbers go to evenArray, odd numbers go to oddArray
+    const {wrongArr, validArr} = data.reduce(
+        (result, current) => {
+            if (
+                !validateForm(current)
+            ) {
+                result.wrongArr.push(current);
+            } else {
+                result.validArr.push(current);
+            }
+            return result;
+        },
+        {wrongArr: [], validArr: []}
+    );
 
     const checkFileExist = fs.existsSync(`${schoolId}.xlsx`);
-    const ws = xlsx.utils.json_to_sheet(data);
-    // console.log(data)
+    const ws = xlsx.utils.json_to_sheet(validArr);
     let wb = {}
     // console.log(checkFileExist)
     if (checkFileExist) {
@@ -43,11 +104,24 @@ app.post('/upload', upload.single('file'), (req, res) => {
     res.json({
         success: true,
         message: 'File uploaded successfully for schoolId: ' + schoolId + '!',
-        data: data
+        data: {
+            inValidArr:  wrongArr,
+            validArr: validArr
+        }
     });
 });
 
 
+app.post('/update',  async (req, res) => {
+    const schoolId = req.query.schoolId;
+    console.log(req)
+    res.json({
+        success: true,
+        message: 'Update successfully for schoolId: ' + schoolId + '!',
+    });
+
+
+    });
 app.get('/distinct', upload.single('file'), async (req, res) => {
     const schoolId = req.query.schoolId;
     const wb = xlsx.readFile(`${schoolId}.xlsx`);
